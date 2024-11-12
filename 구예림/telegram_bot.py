6fac259e3431be5d3e30ded2702d3899ed3a742e
JulGoo <yrruda94@gmail.com>
2024-11-12 17:47:46 +0900

    chatID

--- 구예림/telegram_bot.py
+++ 구예림/telegram_bot.py
@@ -4,15 +4,16 @@ import os
 from dotenv import load_dotenv
 
 
-# 텔레그램 봇 토큰값 가져오기
-def load_token():
+# 텔레그램 봇 토큰값과 채팅 아이디 가져오기
+def load_telegram():
     try:
-        load_dotenv("token.env")
-        return str(os.getenv("token"))
-    # 토큰값을 못 가져왔을 때 예외 처리
+        load_dotenv("telegram.env")
+        token = str(os.getenv("token"))
+        chat_id = str(os.getenv("chatID"))
+        return token, chat_id
+    # 토큰값과 채팅 아이디를 못 가져왔을 때 예외 처리
     except Exception as e:
         print(e)
-        return None
-
+        return None, None
 
 
