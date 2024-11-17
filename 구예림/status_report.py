c49c3d103a7dd4cca7ecea078afe514037e962c2
JulGoo <yrruda94@gmail.com>
2024-11-17 16:56:17 +0900

    status_report

--- 구예림/status_report.py
+++ 구예림/status_report.py
@@ -1,1 +1,18 @@
 # 실시간 상태 보고 기능
+import telegram
+import telegram_bot as tb
+
+# 일반 메세지 전송
+async def send_chat(msg, chat_id):
+    pass
+
+
+# 사진 전송
+async def send_image(chat_id):
+    pass
+
+
+
+# 동영상 전송
+async def send_video(chat_id):
+    pass
