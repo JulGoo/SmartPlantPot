409b74bbd19457460f36d6e05e0d8bdbc3bcff69
WangGeon <73977410+KION126@users.noreply.github.com>
2024-11-30 17:38:08 +0900

    Fix: 통합코드. capture 코드 수정

--- 통합코드/modules/capture.py
+++ 통합코드/modules/capture.py
@@ -1,3 +1,4 @@
+import threading
 import cv2
 import time
 import os
@@ -5,10 +6,10 @@ from influxdb import InfluxDBClient
 from datetime import datetime
 
 # 이미지 저장 경로
-save_directory ="/home/pi/SmartPlantPot/plant_images"
+save_directory = "/home/pi/SmartPlantPot/plant_images"
 
 # 이미지 저장 주기
-capture_interval = 3 * 60 * 60    # (3시간)
+capture_interval = 3 * 60 * 60  # (3시간)
 
 # 주간, 야간
 DAYTIME_START = 6   # 오전 6시
@@ -21,54 +22,56 @@ def is_daytime():
     return DAYTIME_START <= current_hour < DAYTIME_END
 
 def capture_photos_from_webcam():
+    client = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database='spp')
 
-    if is_daytime():
-        client = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database='spp')
+    cap = cv2.VideoCapture(0)
 
-        cap = cv2.VideoCapture(0)
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
 
-        if not os.path.exists(save_directory):
-            os.makedirs(save_directory)
+    if not cap.isOpened():
+        print("capture.py: 웹캠 연결 실패")
+        return
 
-
-        if not cap.isOpened():
-            print("captuer.py: 웹캠 연결 실패")
-            exit()
-
-        try:
-            while True:
+    try:
+        while True:
+            # 주간 시간대인지 확인
+            if is_daytime():
                 timestamp = time.strftime("%Y%m%d_%H%M%S")
                 filename = f"{timestamp}.jpg"
 
                 ret, frame = cap.read()
                 if ret:
+                    # 이미지를 지정된 경로에 저장
                     cv2.imwrite(os.path.join(save_directory, filename), frame)
-                    print(f"captuer.py: 이미지 저장 완료 '/plant_images/{filename}' ")
+                    print(f"capture.py: 이미지 저장 완료 '/plant_images/{filename}' ")
 
+                    # InfluxDB에 데이터 저장
                     json_body = [
-                            {
-                                "measurement":"photos",
-                                "tags":{
-                                    "source":"webcam"
-                                    },
-                                "fields":{
-                                    "filename":filename,
-                                    "timestamp":timestamp
-                                    }
-                                }
-                            ]
+                        {
+                            "measurement": "photos",
+                            "tags": {
+                                "source": "webcam"
+                            },
+                            "fields": {
+                                "filename": filename,
+                                "timestamp": timestamp
+                            }
+                        }
+                    ]
                     client.write_points(json_body)
                 else:
-                    print("captuer.py: 이미지 저장 실패")
+                    print("capture.py: 이미지 저장 실패")
+            else:
+                # 야간 시간대에서 주기적으로 메시지 출력
+                print("capture.py: 야간 시간대 - 웹캠 작동 제한")
 
-                time.sleep(capture_interval)
+            # 주기적으로 이미지 캡처를 시도
+            time.sleep(capture_interval)
 
-        except KeyboardInterrupt:
-            print("captuer.py: KeyboardInterrupt")
+    except KeyboardInterrupt:
+        print("capture.py: KeyboardInterrupt")
 
-        finally:
-            cap.release()
-            cv2.destroyAllWindows()
-
-    else:
-        print("captuer.py: 야간 시간대 - 웹캠 작동 제한")
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
